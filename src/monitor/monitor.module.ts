075a4d1886d5e050915061026773b14d094d990f
Mohsen Bahrami <work.mohsen.bahrami@gmail.com>
2024-09-03 15:03:54 +0330

    fix monitor module and its service as a global

--- src/monitor/monitor.module.ts
+++ src/monitor/monitor.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { MonitorService } from './monitor.service';
 import { MonitorGateway } from './monitor.gateway';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Monitor, MonitorSchema } from './schema/monitor.schema';
 
+@Global()
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Monitor.name, schema: MonitorSchema }]),
   ],
   providers: [MonitorService, MonitorGateway],
+  exports: [MonitorService],
 })
 export class MonitorModule {}
