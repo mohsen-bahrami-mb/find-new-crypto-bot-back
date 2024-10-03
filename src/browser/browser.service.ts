15fd34a11585cac0cdec1f85a555279264d89f2e
Mohsen <mohsen.bahrami7690@gmail.com>
2024-10-03 22:00:55 +0330

    set headless browser on true to remove preview

--- src/browser/browser.service.ts
+++ src/browser/browser.service.ts
@@ -23,7 +23,7 @@ export class BrowserService {
   async initBrowser() {
     try {
       this.browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         executablePath: this.CHROME_APP_PATH,
         userDataDir: './browser-user-data',
       });
